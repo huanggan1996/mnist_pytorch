ce20be6ead19d5c9592c93089d94e1108282b201
huanggan1996 <1367876650@qq.com>
2019-06-14 16:18:27 +0800

    已修改        models/cnnnet.py

--- models/cnnnet.py
+++ models/cnnnet.py
@@ -6,24 +6,25 @@ class CnnNet(nn.Module):
     def __init__(self):
         super(CnnNet, self).__init__()
         self.conv1 = torch.nn.Sequential(
-            torch.nn.Conv2d(3, 32, 3, 1, 1),  # 32x28x28
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(2))  # 32x14x14
+            nn.Conv2d(3, 32, 3, 1, 1),  # 32x28x28
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )  # 32x14x14
         self.conv2 = torch.nn.Sequential(
-            torch.nn.Conv2d(32, 64, 3, 1, 1),  # 64x14x14
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(2)  # 64x7x7
+            nn.Conv2d(32, 64, 3, 1, 1),  # 64x14x14
+            nn.ReLU(),
+            nn.MaxPool2d(2)  # 64x7x7
         )
         self.conv3 = torch.nn.Sequential(
-            torch.nn.Conv2d(64, 64, 3, 1, 1),  # 64x7x7
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(2)  # 64x3x3
+            nn.Conv2d(64, 64, 3, 1, 1),  # 64x7x7
+            nn.ReLU(),
+            nn.MaxPool2d(2)  # 64x3x3
         )
 
         self.dense = torch.nn.Sequential(
-            torch.nn.Linear(64 * 3 * 3, 128),  # fc4 64*3*3 -> 128
-            torch.nn.ReLU(),
-            torch.nn.Linear(128, 10)  # fc5 128->10
+            nn.Linear(64 * 3 * 3, 128),  # fc4 64*3*3 -> 128
+            nn.ReLU(),
+            nn.Linear(128, 10)  # fc5 128->10
         )
 
     def forward(self, x):
